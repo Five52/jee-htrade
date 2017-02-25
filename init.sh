84e7a25e8285a377abbf0838fe9a6d317e7eed7e
Five52 <five52@laposte.net>
2017-02-25 01:48:38 +0100

    Add deployment

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ read -e -p "Enter database host : " dbhost
 read -e -p "Enter database name : " dbname
 read -e -p "Enter username : " username
 read -e -s -p "Enter password : " password
+read -e -p "Enter Glassfish server name : " glassfish
 
 mkdir app/lib
 cd libPerson
@@ -26,3 +27,7 @@ cp dist/libTrade.jar ../app/lib/
 
 cd ../app
 sed -i "s/DB_HOST/${dbhost}/g;s/DB_NAME/${dbname}/g;s/USERNAME/${username}/g;s/PASSWORD/${password}/g" resources/hibernate.cfg.xml
+sed -i "s/GLASSFISH_DOMAIN/${glassfish}/g" resources/hibernate.cfg.xml
+
+asadmin create-domain --domaindir . ${glassfish}
+ant deploy
